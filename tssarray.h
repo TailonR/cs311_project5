12c88ad14e90ba86641fd615dbf1b7feb14b97f5
mperry37 <mperry37@alaska.edu>
2019-10-25 14:17:48 -0800

    fix: moves template to only header

--- tssarray.h
+++ tssarray.h
@@ -159,7 +159,7 @@ public:
 
     // push_back
     // ??? Guarantee
-    void push_back(value_type item)
+    void push_back(const value_type & item)
     {
         insert(end(), item);
     }
@@ -183,6 +183,96 @@ private:
     value_type * _data;      // Pointer to our array
 
 };  // End class TSSArray
+
+// *********************************************************************
+// class TSSArray - Member function definitions
+// *********************************************************************
+
+
+// Copy ctor
+// See header for docs.
+template<typename T>
+TSSArray<T>::TSSArray(const TSSArray<T> & other)
+    :_capacity(other._capacity),
+     _size(other._size),
+     _data(new value_type[other._capacity])
+{
+    std::copy(other.begin(), other.end(), begin());
+}
+
+
+// Move ctor
+// See header for docs.
+template<typename T>
+TSSArray<T>::TSSArray(TSSArray<T> && other) noexcept
+    :_capacity(other._capacity),
+     _size(other._size),
+     _data(other._data)
+{
+    other._capacity = 0;
+    other._size = 0;
+    other._data = nullptr;
+}
+
+
+// Copy assignment operator
+// See header for docs.
+template<typename T>
+TSSArray<T> & TSSArray<T>::operator=(const TSSArray<T> & other)
+{
+    // TODO: WRITE THIS!!!
+    return *this; // DUMMY
+}
+
+
+// Move assignment operator
+// See header for docs.
+template<typename T>
+TSSArray<T> & TSSArray<T>::operator=(TSSArray && other) noexcept
+{
+    // TODO: WRITE THIS!!!
+    return *this; // DUMMY
+}
+
+
+// resize
+// See header for docs.
+template<typename T>
+void TSSArray<T>::resize(size_type newsize)
+{
+    // TODO: WRITE THIS!!!
+}
+
+
+// insert
+// See header for docs.
+template<typename T>
+typename TSSArray<T>::iterator TSSArray<T>::insert(TSSArray<T>::iterator pos,
+                                  const TSSArray<T>::value_type & item)
+{
+    // TODO: WRITE THIS!!!
+    return begin();  // DUMMY
+}
+
+
+// erase
+// See header for docs.
+template<typename T>
+typename TSSArray<T>::iterator TSSArray<T>::erase(TSSArray<T>::iterator pos)
+{
+    // TODO: WRITE THIS!!!
+    return begin();  // DUMMY
+}
+
+
+// swap
+// See header for docs.
+template<typename T>
+void TSSArray<T>::swap(TSSArray<T> & other) noexcept
+{
+    // TODO: WRITE THIS!!!
+}
+
 
 
 #endif //#ifndef FILE_TSSArray_H_INCLUDED
